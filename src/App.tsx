cd2f0b0c0b867a3193883d997592c9edb75305a8
Ben Christel <benchristel@users.noreply.github.com>
2022-12-29 09:25:23 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./app.css"
+
+export function App() {
+  return <h1>Hello from Preact!</h1>
+}
+
+test("App", {
+  "renders a greeting"() {
+    expect(App().props.children, equals, "Hello from Preact!")
+  },
+})
